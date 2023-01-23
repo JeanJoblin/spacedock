2c06b133ae96276718386b300352a3bf9f1ec942
JeanJoblin <invisibleorbs@gmail.com>
2023-01-23 17:15:57 -0500

    make hull.json

--- src/app/resources/tableParser.js
+++ src/app/resources/tableParser.js
@@ -9,23 +9,42 @@ function readTable (path) {
 };
 
 const assignTTO = (imp, keys) => {
+  //Assigning two strings to be keys for object and values
   const titles = keys.split(' ');
   const lineEntries = imp.split(/\r/);
-  console.log(lineEntries);
+  //Map table entries (obj values) to new array split by newlines
   const entries = lineEntries.map( (entry) => {
-    console.log('within map:', entry);
+    //splitting table entries by spaces
     const newEntry = entry.split(' ');
-    console.log('after Split: ', newEntry);
     return newEntry;
   });
-  console.log('split titles:', titles);
-  console.log('split entries:', entries);
+  //create a new object, then assign it empty keys named as the first entries in the input table
+  let obj = {};
+  for (let i = 0; i < entries.length; i++) {
+    obj[entries[i][0]] = {};
+    for (let j = 1; j < titles.length; j++) {
+      obj[entries[i][0]][titles[j]] = entries[i][j];
+    }
+  }
+  // for(let i = 0; i < titles.length; i++) {
+  //     newObj[titles[i]] = entry[i];
+  // }
+  return obj;
 };
 
 const hullClasses = readTable('./HullClasses.txt');
 var hullClassKeys = readTable('./hullClassKeys.txt');
-hullClassKeys.toLowerCase();
-console.log(hullClasses);
-assignTTO(hullClasses, hullClassKeys);
+const hulls = assignTTO(hullClasses, hullClassKeys);
+console.log(hulls.StrikeFighter);
+
+var json = JSON.stringify(hulls);
+fs.writeFile('hull.json', json, (err) => {
+  if (err) throw err;
+  console.log('File has been saved!');
+});
+
+
+
+
 
 
