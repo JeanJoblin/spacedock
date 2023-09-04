a7a98034ba43f04fde268304aec8f6f1e5f5ad9a
JeanJoblin <invisibleorbs@gmail.com>
2023-09-04 12:33:58 -0400

    Remove require from unused module

--- src/app/resources/tables.js
+++ src/app/resources/tables.js
@@ -2,7 +2,6 @@ export const hulls = require('../../app/resources/hulls.json');
 export const fittings = require('../../app/resources/fittings.json');
 export const defenses = require('../../app/resources/defenses.json');
 export const weapons = require('../../app/resources/weapons.json');
-export const stationHulls = require('../../app/resources/stations.json');
 
 const tables = {
   hulls: hulls,
